eabc1095a4d91180d72ac6b7322daa23d2331368
breno <osvaldobreno99@gmail.com>
2019-05-03 14:37:42 -0300

    Regular Studie

---
+++ OtherDoc/SO_file.py
@@ -0,0 +1,58 @@
+#
+# Types of Files
+#
+# Regular File
+# Directory
+# File of Special Characters
+# File of Special Blocks
+
+# dois tipos de arquivos regulares
+
+# --> ASCII
+#         apropriado para pipelines e arquivos que tem como entrada saída de outro programa
+# --> binario
+#         tem um formato exato
+#         5 seções:
+#         --> head
+#         -->text
+#         -->data
+#         -->bytes of realocation
+#         -->simble table
+#         {exe ou repositorio}
+
+#       Acess Method
+# 33154693 --> pos grad
+#       --> Sequencial Acess
+#       -->Random Acess
+#            + good for database files
+
+    # Atributos de arquivo
+    #  All files has name and data
+
+    #  SO associa varios atributos para o arquivo --> são dneominados de attr ou metadados
+
+    # bits sinalização são campos curtos que controlam ou habilitam alguma propriedade especifica
+
+    # sinalização de arquivamneto é um bit que controle se foi feito um backup
+
+    # sinalização temporária permite que um arquivo seja marcado para remoção
+
+
+    # Operações com arquivos
+
+    # Create --> criar o arquivo sem nada dentro
+    # Dekete --> remoção do arquivp
+    # Open --> abri o arquivo antes de usar
+    # Close --> fecha
+    # Read --> leitura
+    # Write --> dados escretos m um novo arquivo(sobrescrição)
+    # Append --> Acrescenta dados no final do arquivo
+    # Seek --> para arquivos de acesso aleatório (posicionamento do ponteiro)
+    # get attribute --> ler os atributos de um arquivo (utilizado pelo make para para unir
+    #     arquivos fontes e objetos  e organiza o numero minimo de compilações
+    #     necessárias para atualizar)
+    # set attribute --> permite ao usuário modificar atributos comuns
+
+
+
+
