7ab1af515bd952ec7bbecc2d08dd20b47a939343
breno <osvaldobreno99@gmail.com>
2019-05-03 13:34:52 -0300

    Second Part

--- DAO/Connection.py
+++ DAO/Connection.py
@@ -8,9 +8,3 @@ class Connection:
         con = psycopg2.connect(host='x', database='cbioportal', user='postgres', password='x')
 
         return con
-
-    # @staticmethod
-    # def get_connection():
-    #     con = .o(host='143.107.223.173', database='cbioportal', user='postgres', password='root')
-    #
-    #     return con
